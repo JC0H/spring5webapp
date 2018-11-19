3f2bb4648c06961699f23b1ab455f5dfc8037e74
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-11-19 15:10:01 +0100

    Initial commit

---
+++ src/main/java/ua/com/jcoh/spring5webapp/model/Author.java
@@ -0,0 +1,64 @@
+package ua.com.jcoh.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private List<Books> books = new ArrayList<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, List<Books> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Books> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Books> books) {
+        this.books = books;
+    }
+}
